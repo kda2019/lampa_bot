724cbd7b35946be5064bcf3b91c4142d59775de2
Дмитрий Кравченко <kda2019@gmail.com>
2021-04-26 02:09:45 +0300

    0.9.1 Beta

--- actions.py
+++ actions.py
@@ -43,3 +43,41 @@ async def check_top_lampovyh_cats(bot, message):
         text += f"{counter}) <a href='tg://user?id={i.user_id}'>{user_.user.first_name} </a> - {i.amount}\n"
         counter += 1
     await bot.send_message(message.chat.id, text, parse_mode='HTML')
+
+
+async def mute_user(bot, message):
+    if (await bot.get_chat_member(message.chat.id, 954054488)).status != "administrator":
+        await bot.send_message(message.chat.id, 'Я не смогу это сделать пока не стану админом :(')
+
+    elif message.reply_to_message.from_user.is_bot:
+        await bot.send_message(message.chat.id, 'Зачем ты пытаешься поразить бота? Он бесчувственный пустой мешок нулей и единиц')
+
+    elif (await bot.get_chat_member(message.chat.id, message.reply_to_message.from_user.id)).status in ["administrator", "creator"]:
+        await bot.send_message(message.chat.id, f"Наши админы настолько ламповые, что круглосуточно восхищаются сами собой. Тебе не удалось поразить {message.reply_to_message.from_user.first_name}")
+
+    else:
+        user = UserModel.get_or_create(chat=ChatModel.get_or_create(chat_id=message.chat.id)[0], user_id=message.from_user.id)[0]
+        max_time_mute = user.amount ** (1 / 2)
+
+        try:
+            time_mute = int(message.text.split()[1])
+        except:
+            time_mute = max_time_mute
+
+        if time_mute > max_time_mute:
+            time_mute = max_time_mute
+        elif time_mute < 1:
+            await bot.send_message(message.chat.id, 'Вы недостаточно ламповы чтобы кого-то замутить :(')
+            return
+
+        if (await bot.get_chat_member(message.chat.id, message.reply_to_message.from_user.id)).status == "restricted":
+            await bot.send_message(message.chat.id, f'{message.reply_to_message.from_user.first_name} уже поражен и не может сказать ни слова.')
+
+        else:
+            dt_mute = datetime.timedelta(minutes=time_mute)
+            await bot.restrict_chat_member(message.chat.id, message.reply_to_message.from_user.id, until_date=dt_mute)
+            await bot.send_message(message.chat.id, f'{message.from_user.first_name} поразил своей ламповостью кота по имени {message.reply_to_message.from_user.first_name}.\n{message.reply_to_message.from_user.first_name} восхищен и не сможет сказать ни слова {dt_mute.seconds//60} мин. {dt_mute.seconds%60} сек.')
+            user.amount -= int(time_mute)
+            user.save()
+
+
--- main.py
+++ main.py
@@ -12,20 +12,14 @@ data = {}
 
 @dp.message_handler(lambda message: message.chat.id != message.from_user.id)
 async def group_messages(message):
-    x = await bot.get_chat(message.from_user.id)
-    print(x)
     if message.text == '/eat_shawarma':
         await eat_shawarma(bot, message)
-    if message.text == '/check_my_lampovost':
+    elif message.text == '/check_my_lampovost':
         await check_my_lampovost(bot, message)
-    if message.text == '/check_top_lampovyh_cats':
+    elif message.text == '/check_top_lampovyh_cats':
         await check_top_lampovyh_cats(bot, message)
-    if message.text == '/test':
-        x = await bot.get_chat_member(message.chat.id, message.from_user.id)
-        print(x.user.first_name)
-        caption = f"<a href='tg://user?id={message.from_user.id}'>{x.user.first_name} </a> - Красавчик"
-        await bot.send_message(message.chat.id, caption, parse_mode='HTML')
-
+    elif message.reply_to_message is not None and message.text.split()[0].lower() == 'мут':
+        await mute_user(bot, message)
 
 @dp.message_handler(lambda message: message.chat.id == message.from_user.id)
 async def group_messages(message):
